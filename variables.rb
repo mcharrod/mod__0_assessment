16d7f46c8ba96282f830c57165b99bfbb7959e22
Katy Harrod <m.catherine.harrod@gmail.com>
2021-09-22 19:44:00 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+movie = "Legally Blonde"
+is_entertaining = true
+movie = "Old Boy"
+
+
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+
+
+favorite_foods = ["sushi", "pizza", "ramen"]
+
+favorite_foods.delete_at(0)
+
+puts movie.length
