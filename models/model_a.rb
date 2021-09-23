4711e828a21e4eb9db600d0f02478259995f6db5
Katy Harrod <m.catherine.harrod@gmail.com>
2021-09-22 19:56:17 -0600

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+t_rex = {
+  favorite_snack: "dinosaurs",
+  nemesis: "meteor",
+  weight_in_lbs: 15000,
+  number_of_teeth: 60,
+  can_clap: false,
+  is_cuddly: false,
+  hobbies: ["screeching", "stomping", "terrorizing", "stargazing"],
+  dislikes: ["mosquitos", "velociraptors", "tar", "caves"]
+}
