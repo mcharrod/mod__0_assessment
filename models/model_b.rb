de6d05028904bd3be7aea19a2eab8afc0a653179
Katy Harrod <m.catherine.harrod@gmail.com>
2021-09-22 20:11:00 -0600

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: "Destiny",
+  favorite_food: "cake",
+  screens: 2,
+  current_projects: 3,
+  tabs_open: ["Slack", "Terminal", "Atom", "Github", "Google"],
+  office_supplies: ["Computer", "Headset", "Snack", "Sticky notes"],
+  works_remotely: true,
+  viewing_screen: true
+}
